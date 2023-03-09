9c813a09632bf15e2373e9ecd8b455adbaef05f1
eliotch <eliotc@nycstudents.net>
2023-03-09 09:32:46 -0500

    updated methods

--- src/Board.java
+++ src/Board.java
@@ -11,14 +11,36 @@ public class Board {
         board[r-1][c-1]=num;
     }
 
-    public static boolean isValid(int r, int c) {
-        r--;
-        c--;
+    public boolean isValid(int r, int c) {
         boolean valid=false;
         if((r>=1&&r<=5)&&(c>=1&&c<=5)) {
             valid=true;
+        }else{
+            return false;
         }
-        if()
-        return false;
+        if(board[r-1][c-1]==0){
+            valid=true;
+        }else{
+            return false;
+        }
+        return valid;
+    }
+
+    public int sum(){
+        int rowSum=0;
+        int colSum=0;
+        for(int i=0; i<board.length; i++){
+            int currentNum=board[i][0];
+            for(int j=0; j<board[0].length-1; j++){
+                rowSum+=currentNum; //fix this
+                if(board[i][j+1]==currentNum){
+                    rowSum+=currentNum;
+                }else{
+                    rowSum-=currentNum;
+                    currentNum=board[i][j];
+                }
+            }
+        }
+        return rowSum+colSum;
     }
 }
--- src/PlayGame.java
+++ src/PlayGame.java
@@ -1,36 +1,48 @@
 import java.util.Scanner;
 import java.util.Arrays;
 public class PlayGame {
-    public static void main(String args[]){
-        Scanner scan=new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to Neighbors");
-        Board board=new Board();
-        for(int[] a: board.getBoard()){
-            System.out.println(Arrays.toString(a));
+        Board board = new Board();
+        for (int[] a : board.getBoard()) {
+            System.out.println("-------------------------------");
+            System.out.print("|");
+            for (int b : a) {
+                System.out.printf("%3d  |", b);
             }
-
-        for(int i=0; i<25; i++){
-            int num=Dice.randNum();
-            System.out.println("number "+ (i+1)+": "+ num);
-            System.out.println("Enter row number (1-5): ");
-            int row=scan.nextInt();
-            System.out.println("Enter column number (1-5): ");
-            int col=scan.nextInt();
-            if(board.isValid(row, col)){
-                board.setBoard(row, col, num);
-                for(int[] a: board.getBoard()){
-                    System.out.println(Arrays.toString(a));
+            System.out.println();
+        }
+        System.out.println("-------------------------------");
+            for (int i = 0; i < board.getBoard().length*board.getBoard()[0].length; i++) {
+                int num = Dice.randNum();
+                System.out.println("number " + (i + 1) + ": " + num);
+                System.out.println("Enter row number (1-"+board.getBoard().length+"): ");
+                int row = scan.nextInt();
+                System.out.println("Enter column number (1-"+board.getBoard()[0].length+"): ");
+                int col = scan.nextInt();
+                if (board.isValid(row, col)) {
+                    board.setBoard(row, col, num);
+                } else {
+                    while (!board.isValid(row, col)) {
+                        System.out.println("That spot is either taken or invalid, try again: ");
+                        System.out.println("number " + (i + 1) + ": " + num);
+                        System.out.println("Enter row number (1-5): ");
+                        row = scan.nextInt();
+                        System.out.println("Enter column number (1-5): ");
+                        col = scan.nextInt();
+                    }
                 }
-            }else{
-                while(!board.isValid(row, col)) {
-                    System.out.println("That spot is either taken or invalid, try again: ");
-                    System.out.println("number " + (i + 1) + ": " + num);
-                    System.out.println("Enter row number (1-5): ");
-                    row = scan.nextInt();
-                    System.out.println("Enter column number (1-5): ");
-                    col = scan.nextInt();
+                for (int[] a : board.getBoard()) {
+                    System.out.println("-------------------------------");
+                    System.out.print("|");
+                    for (int b : a) {
+                        System.out.printf("%3d  |", b);
+                    }
+                    System.out.println();
                 }
+                System.out.println("-------------------------------");
             }
         }
     }
-}
+
