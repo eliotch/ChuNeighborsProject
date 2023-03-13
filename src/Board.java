d0f2c9cbb706444da68c3cb296d2fdb1951cace8
BT_3W2_25 <eliotc@nycstudents.net>
2023-03-13 09:31:44 -0400

    updated sum()

--- src/Board.java
+++ src/Board.java
@@ -33,32 +33,31 @@ public class Board {
     public int sum(){
         int rowSum=0;
         int colSum=0;
-        int rowCount=0;
-        int colCount=0;
         for(int i=0; i<board.length; i++){
             int currentNum=board[i][0];
-            for(int j=0; j<board[0].length-1; j++){
-                if(board[i][j+1]==currentNum){
-                    rowCount++;
+            int rowCount=1;
+            for(int j=1; j<board[0].length; j++){
+                rowSum+=currentNum;
+                if(board[i][j]==currentNum){
+                    rowSum-=currentNum;
+                    System.out.println(rowSum);
                 }else{
-                    if(rowCount>1){
-                        rowSum+=rowCount*currentNum;
-                    }
-                    rowCount=0;
-                    currentNum=board[i][j+1];
+                    currentNum=board[i][j];
                 }
             }
         }
         for(int i=0; i<board[0].length-1; i++){
             int currentNum=board[0][i];
+            int colCount=1;
             for(int j=0; j<board.length-1; j++){
                 if(board[j+1][i]==currentNum){
                     colCount++;
                 }else{
                     if(colCount>1){
                         colSum+=colCount*currentNum;
+                        System.out.println(colCount);
                     }
-                    colCount=0;
+                    colCount=1;
                     currentNum=board[j][i+1];
                 }
             }
